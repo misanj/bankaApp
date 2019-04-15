cb869f8316b18d0089044234e81b8f9f2772b11d
temisan otokuefor <temipretty1992@gmail.com>
2019-04-15 09:04:26 +0100

    ft(admin-staff can delete account):this adds delete account api endpoint to banka Worked on account routes Wrote test for delete account [Finishes #165335584]

--- server/api/controllers/acctController.js
+++ server/api/controllers/acctController.js
@@ -50,5 +50,28 @@ class AccoountController {
       error: `Account ${accountNumber} does not exist`,
     });
   }
+
+  // eslint-disable-next-line consistent-return
+  static deleteAccount(req, res) {
+    const { accountNumber } = req.params;
+    const validAccount = accounts
+      .find(eachAccount => eachAccount.accountNumber === parseInt(accountNumber, 10));
+    if (!validAccount) {
+      return res.status(404).json({
+        status: res.statusCode,
+        error: `Account ${accountNumber} does not exist`,
+      });
+    }
+    accounts.forEach((account) => {
+      if (account.accountNumber === parseInt(accountNumber, 10)) {
+        const indexNumber = accounts.indexOf(account);
+        accounts.splice(indexNumber, 1);
+      }
+      return res.status(200).json({
+        status: res.statusCode,
+        message: 'Account successfully deleted!',
+      });
+    });
+  }
 }
 export default AccoountController;
